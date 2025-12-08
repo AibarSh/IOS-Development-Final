5e1b627268c5a41d4faeb4f0dfa54f04501e8903
Aibar <a_shakeyev@kbtu.kz>
2025-12-08 19:40:38 +0500

    Project creation

---
+++ AShVelo/AShVelo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AShVelo
+//
+//  Created by Macbook Air on 08.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
